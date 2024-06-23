5cd8e99c5d68e0b53be3e74281da2f4365bbf559
cybardev <sheikh@cybar.dev>
2024-06-23 15:38:41 -0300

    re: simplify script

--- Tools/rg35xxplus/Clear Recently Played.pak/launch.sh
+++ Tools/rg35xxplus/Clear Recently Played.pak/launch.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # AUTHOR: shauninman <github.com/shauninman>
-# CO-AUTHOR: cybardev <sheikh@cybar.dev>
 
-rm -f "$SHARED_USERDATA_PATH/.minui/recent.txt"
-sync
+rm "$SHARED_USERDATA_PATH/.minui/recent.txt"
