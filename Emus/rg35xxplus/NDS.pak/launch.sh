fca8372c35af083efafc8df4163b78ee9cb13e75
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-10 10:22:23 -0600

    NDS: check for presence of lcd_run function rather than device ID to determine which function to run, to accommodate both old and new versions of 28xx stock OS

--- Emus/rg35xxplus/NDS.pak/launch.sh
+++ Emus/rg35xxplus/NDS.pak/launch.sh
@@ -43,11 +43,11 @@ monitor_for_kill &
 
 export LD_LIBRARY_PATH="/usr/lib32:/usr/lib:$LD_LIBRARY_PATH"
 
-if grep -q "RG28xx" /mnt/vendor/oem/board.ini; then
-	/mnt/vendor/ctrl/setNDS.sh savedir /mnt/sdcard;
+/mnt/vendor/ctrl/setNDS.sh savedir /mnt/sdcard;
+
+if grep -q "lcd_run" /mnt/vendor/ctrl/setNDS.sh; then
 	/mnt/vendor/ctrl/setNDS.sh lcd_run "$ROM" > "$LOGS_PATH/$EMU_TAG.txt" 2>&1;
 else
-	/mnt/vendor/ctrl/setNDS.sh savedir /mnt/sdcard;
 	/mnt/vendor/ctrl/setNDS.sh run "$ROM" > "$LOGS_PATH/$EMU_TAG.txt" 2>&1;
 fi
 
